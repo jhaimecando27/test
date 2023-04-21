c8d9d33303e5408ea01791f803ccdb1380105267
Jhaime Cando <jhaimecando27@gmail.com>
2023-04-21 19:55:13 +0800

    feat(inventory): add models

--- Bevrecs/inventory/models.py
+++ Bevrecs/inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+STOCK_CATEGORY = (
+    # Left, For database para hindi mahaba masyado. Tas trip ko
+    # Right, For user interface
+    ("INGRI", "Ingridients"),
+    ("AO", "Add-Ons"),
+    ("UT", "Utensils"),
+)
+
+
+class Stocks(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(choices=STOCK_CATEGORY, max_length=10)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.FloatField()
+    date_in = models.DateField()
+    expiration = models.DateField()
+
+    def __str__(self):
+        return self.name
